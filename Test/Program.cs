a2b1e90a13428c958f49ed67e1262b4edf28b537
XBB <icexb_baka@163.com>
2024-03-18 21:13:38 +0800

    测试项目

---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SekaiToolsCore;
+
+var config = new VideoProcessTaskConfig("",
+    @"D:\ProjectSekai\Archive\event_124_wl_ws\ev_124_01.mp4",
+    @"D:\ProjectSekai\Archive\event_124_wl_ws\wl_wonder_01_01.json");
+config.SetSubtitleTyperSetting(50, 80);
+
+var task = new VideoProcess(config);
+
+task.Process();
