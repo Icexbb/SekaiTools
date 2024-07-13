7eef6fab7a868e9e43e28a393ee4cecb232aa4e1
XBB <icexb_baka@163.com>
2024-07-14 04:05:09 +0800

    chore: bump version to 1.2.5.0714

--- SekaiToolsGUI/AssemblyInfo.cs
+++ SekaiToolsGUI/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Windows;
     // app, or any theme specific resource dictionaries)
 )]
 [assembly: SupportedOSPlatform("windows")]
-[assembly: AssemblyVersion("1.2.4.0707")]
+[assembly: AssemblyVersion("1.2.5.0714")]
