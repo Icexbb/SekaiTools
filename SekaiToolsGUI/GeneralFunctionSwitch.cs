3ae6843eed52a29034f23f54df4ab93767aee5f1
XBB <icexb_baka@163.com>
2026-08-07 17:03:17 +0800

    fix: 启用事件时间线功能

--- SekaiToolsGUI/GeneralFunctionSwitch.cs
+++ SekaiToolsGUI/GeneralFunctionSwitch.cs
@@ -3,6 +3,6 @@ namespace SekaiToolsGUI;
 /*全局功能开关*/
 public static class GeneralFunctionSwitch
 {
-    public static bool EventTimeline => false;
+    public static bool EventTimeline => true;
     public static bool EventPlayBack => false;
 }
