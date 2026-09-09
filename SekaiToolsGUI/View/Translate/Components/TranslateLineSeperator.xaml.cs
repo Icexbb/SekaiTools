c8668dd97d2686810fadaa172eb8cb475516cd9f
XBB <icexb_baka@163.com>
2026-09-09 16:32:26 +0800

    feat(translate): add TranslateLineSeparator component and refactor TranslateLineDialog layout

---
+++ SekaiToolsGUI/View/Translate/Components/TranslateLineSeperator.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using SekaiToolsGUI.ViewModel.Translate;
+using Wpf.Ui.Abstractions.Controls;
+
+namespace SekaiToolsGUI.View.Translate.Components;
+
+public partial class TranslateLineSeparator : UserControl
+{
+    public TranslateLineSeparator()
+    {
+        InitializeComponent();
+    }
+}
